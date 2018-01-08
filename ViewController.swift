e0e88ae77e163b5a3cd28fe4440f04aa3de007fd
Julius Saputra <julius_saputra@hotmail.com>
2018-01-08 11:59:09 +0100

    adding framework JWTswift

--- ViewController.swift
+++ ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JWTswift
 
 class ViewController: UIViewController {
     
@@ -39,10 +40,11 @@ class ViewController: UIViewController {
         
         
 //        requester.fetch(url: reqURL!, requestData: self.requestData)
+        /*
         var header : [String : Any] = [:]
         header["typ"] = "JWT"
         header["alg"] = "RS256"
-        var payload : [String : Any] = [
+        let payload : [String : Any] = [
             "iss" : "julius",
             "exp" : 2020,
             "link" : "http://google.ch"
@@ -51,7 +53,7 @@ class ViewController: UIViewController {
         let jws = JWS()
         let jwt = jws.sign(header: header, payload: payload, key: privateKey!)
         print("JWT : " , jwt!)
-        
+        */
          /*
         let urlPath = Bundle.main.url(forResource: "rsaCert", withExtension: ".der")
         print("url path : " , urlPath?.absoluteString as Any)
@@ -61,7 +63,7 @@ class ViewController: UIViewController {
         let verified = jws.verify(header: header, payload: payload, signature: &jws.signatureStr!, key: publickey!)
         print(verified)
         */
-        
+        self.testJWTswift()
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,4 +71,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func testJWTswift() {
+        var dict = [
+            "e"  : "AQAB",
+            "kty" : "RSA",
+            "n" : "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw"]
+        let kid = KeyStore.createKIDfromJWK(jwkDict: dict) as String!
+        print("KID : \(String(describing: kid!))")
+        dict["kid"] = kid!
+        let keyStore = KeyStore.init()
+        let key = keyStore.jwkToKey(jwkDict: dict)
+        _ = KeyChain.deleteKey(tagString: "testKey", keyToDelete: key!)
+        let saved  = KeyChain.saveKey(tagString: "testKey", keyToSave: key!)
+        print("SAVED : \(saved)")
+        //load the kid from keychain
+        let loadKey = KeyChain.loadKey(tagString: "testKey")
+        print("load key : \(loadKey!.getKeyObject())")
+        print("kid and loadKid same : \(kid! == loadKey?.getKid()!)")
+        
+    }
 }
